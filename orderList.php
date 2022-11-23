8fcf204e4439c697ac6eec88a32956a1f64641e0
koPryce <prycekevaughn@gmail.com>
2022-11-22 23:03:02 -0500

    Final Commit

--- orderList.php
+++ orderList.php
@@ -33,54 +33,43 @@
                             </thead>
                             <tbody>
                                 <?php 
-                                    $query = "SELECT * FROM orders LIMIT $start_from, $num_per_page";
+                                    if (strcmp($_SESSION['user-type'], 'vendor') == 0){
+                                        $vendor_id = $_SESSION['user_id'];
+                                        $query = "SELECT * FROM orders 
+                                        INNER JOIN products ON orders.product_id = products.product_id
+                                        WHERE products.vendor_id = '$vendor_id'
+                                        LIMIT $start_from, $num_per_page";
+                                    }else{
+                                        $query = "SELECT * FROM orders LIMIT $start_from, $num_per_page";
+                                    }
                                     $orders = mysqli_query($con, $query);
-                                    $count = 0;
 
                                     if(mysqli_num_rows($orders) > 0){
                                         foreach($orders as $order){
-                                             $id = $order['product_id'];
-                                            $product_query = "SELECT * FROM products 
-                                            WHERE product_id='$id'";
-                                            $product_info = mysqli_query($con, $product_query);
-
-                                            if (mysqli_num_rows($product_info) > 0){
-                                                foreach($product_info as $info){
-                                                    $productName = $info['product_name'];
-                                                    $productCode = $info['product_code'];
-                                                    $productType = $info['product_type'];
-                                                    $productSalesPrice = $info['sales_price'];
-                                                    $vendorID = $info['vendor_id'];
-                                                }
-                                            }else{
-                                                $vendorID = 0;
-                                            }
-
-                                            if(strcmp($_SESSION['user-type'], "vendor") == 0){
-                                                if($vendorID != $_SESSION['user_id']){
-                                                    continue;
+                                            $product_id = $order['product_id'];
+                                            $product_info = "SELECT * FROM products WHERE product_id = '$product_id'";
+                                            $product = mysqli_query($con, $product_info);
+                                            if(mysqli_num_rows($product) > 0){
+                                                foreach($product as $prod){
+                                                    $productName = $prod['product_name'];
+                                                    $productCode = $prod['product_code'];
+                                                    $productType = $prod['product_type'];
+                                                    $salesPrice = $prod['sales_price'];
                                                 }
                                             }
-                                            
-                                            $count+= 1;
                                 ?>
                                 <tr>
                                     <td><?= $productName; ?></td>
                                     <td><?= $productCode; ?></td>
-                                    <td><?=  $productType; ?></td>
-                                    <td><?= $productSalesPrice; ?></td>
+                                    <td><?= $productType; ?></td>
+                                    <td><?= $salesPrice; ?></td>
                                     <td><?= $order['customer_name']; ?></td>
                                     <td><?= $order['amount']; ?></td>
                                     <td><?= $order['total']; ?></td>
                                 </tr>
                                 <?php
-                                            
                                         }
                                     }else{
-                                        echo "<h5> No Orders Found </h5>";
-                                    }
-
-                                    if ($count == 0) {
                                         echo "<h5> No Orders Found </h5>";
                                     }
                                 ?>
@@ -91,8 +80,15 @@
             </div>
         </div>
         <?php
-            $product_query = "SELECT * FROM orders";
-            $orders = mysqli_query($con, $product_query);
+            if (strcmp($_SESSION['user-type'], 'vendor') == 0){
+                $vendor_id = $_SESSION['user_id'];
+                $query = "SELECT * FROM orders 
+                INNER JOIN products ON orders.product_id = products.product_id
+                WHERE products.vendor_id = '$vendor_id'";
+            }else{
+                $query = "SELECT * FROM orders";
+            }
+            $orders = mysqli_query($con, $query);
             
             $total_records = mysqli_num_rows($orders);
             $total_pages = ceil($total_records/$num_per_page);
