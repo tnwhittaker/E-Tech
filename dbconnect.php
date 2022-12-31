b3745b2e252e88ecfa95cdbb3daf45f7a6a3f449
Torrike Whittaker <torrikedev@gmail.com>
2022-12-31 03:14:19 -0500

    make it better

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,18 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","wsdi_db");
+// $con = mysqli_connect("localhost","root","","wsdi_db");
 
-if(!$con){
-    die('Connection Failed'. mysqli_connect_error());
-}
+// if(!$con){
+//     die('Connection Failed'. mysqli_connect_error());
+// }
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
